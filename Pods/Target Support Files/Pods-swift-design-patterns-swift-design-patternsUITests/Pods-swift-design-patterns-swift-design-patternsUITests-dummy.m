ab999ca2ee0d1b99a8ad1b1ffa5922188e6828fb
jordg2015 <jordy.g2012@gmail.com>
2021-05-08 00:34:29 -0600

    feat(core): perform request wrapper added

--- Pods/Target Support Files/Pods-swift-design-patterns-swift-design-patternsUITests/Pods-swift-design-patterns-swift-design-patternsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_design_patterns_swift_design_patternsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_design_patterns_swift_design_patternsUITests
-@end
--- swift-design-patterns/ViewController.swift
+++ swift-design-patterns/ViewController.swift
@@ -11,7 +11,22 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    HttpClient.default.performRequest(to: "https://api.themoviedb.org/3/discover/movie?api_key=f2b566ae4212940dc51544f3986bd3ca&primary_release_year=2021&page=1", httpMethod: .Get, keyPath: "results", body: nil) { (movies: [Movie]?, error) in
+      if let movieList = movies {
+        movieList.forEach { (movie) in
+          print(movie)
+        }
+      }
+      
+      if let e = error {
+        let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default)
+        alert.addAction(action)
+        self.present(alert, animated: true)
+        
+      }
+    }
+    
   }
 
 
